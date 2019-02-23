e09eb81ff8bf7bc99bbf4f0d2158a27927575251
rekwet <mydevshop@gmail.com>
2019-02-23 18:19:54 +0200

    create pages working

--- models/genre.js
+++ models/genre.js
@@ -3,16 +3,15 @@ let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
 let GenreSchema = new Schema({
-    name : {type: String, required: true, min: [3], max: [100]}
+  name: { type: String, required: true, min: [3], max: [100] }
 })
-
 
 // Virtual for genre's URL
 GenreSchema
-.virtual('url')
-.get(function () {
-  return '/catalog/genre/' + this._id;
-});
+  .virtual('url')
+  .get(function () {
+    return '/catalog/genre/' + this._id;
+  });
 
-//Export model
+// Export model
 module.exports = mongoose.model('Genre', GenreSchema);
