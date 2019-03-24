fb74e6567af0b758756130731053d8066d8ad486
rekwet <mydevshop@gmail.com>
2019-03-24 10:44:55 +0200

    v1

--- app.js
+++ app.js
@@ -1,59 +1,65 @@
-let createError = require('http-errors');
-let express = require('express');
-let path = require('path');
-let cookieParser = require('cookie-parser');
-let logger = require('morgan');
+let createError = require('http-errors')
+let express = require('express')
+let path = require('path')
+let cookieParser = require('cookie-parser')
+let logger = require('morgan')
+
+// Use Helmet to protect against well known vulnerabilities
+let helmet = require('helmet')
 
 // add database details to setup
 // let dbInstance = require('./db/dbcon');
 
-let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
-let catalogRouter = require('./routes/catalog'); // Import routes for "catalog" area of site
+let indexRouter = require('./routes/index')
+let usersRouter = require('./routes/users')
+let catalogRouter = require('./routes/catalog') // Import routes for "catalog" area of site
 
 let app = express()
 
+// use Helmet to activate the middelware and protect the app.
+app.use(helmet())
+
 // Set up mongoose connection
-let mongoose = require('mongoose');
-let mongoDB = 'mongodb://dba:mongodbadmin1@ds137404.mlab.com:37404/lf_local_library';
-mongoose.connect(mongoDB, { useNewUrlParser: true });
-mongoose.Promise = global.Promise;
-let db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+let mongoose = require('mongoose')
+let mongoDB = 'mongodb://dba:mongodbadmin1@ds137404.mlab.com:37404/lf_local_library'
+mongoose.connect(mongoDB, { useNewUrlParser: true })
+mongoose.Promise = global.Promise
+let db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 
 // add middleware libraries
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 // get Express to server all static files in the ./public dir
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
 // routes added to middleware stack
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/catalog', catalogRouter);
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
+app.use('/catalog', catalogRouter)
 
 // add middleware handlers methods for errors and 404 responses
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
-});
+  next(createError(404))
+})
 
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status(err.status || 500)
+  res.render('error')
+})
 
 // add app let to the exports module so we can import in /bin/www
-module.exports = app;
+module.exports = app
--- db/dbcon.js
+++ db/dbcon.js
@@ -1,12 +1,22 @@
-//mongoose sql ide for mongodb
-let mongoose = require('mongoose');
-//DB middleware setup
-//Set up default mongoose connection
-let mongoDB = 'mongodb://127.0.0.1/my_database';
-mongoose.connect(mongoDB);
+// mongoose sql ide for mongodb
+let mongoose = require('mongoose')
+// DB middleware setup
+// Set up default mongoose connection
+let mongoDB = 'mongodb://127.0.0.1/my_database'
+mongoose.connect(mongoDB)
 // Get Mongoose to use the global promise library
-mongoose.Promise = global.Promise;
-//Get the default connection
-let db = mongoose.connect;
-//Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+mongoose.Promise = global.Promise
+// Get the default connection
+let db = mongoose.connect
+// Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+/*
+let mongoose = require('mongoose')
+let mongoDB = 'mongodb://dba:mongodbadmin1@ds137404.mlab.com:37404/lf_local_library'
+mongoose.connect(mongoDB, { useNewUrlParser: true })
+mongoose.Promise = global.Promise
+let db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+*/
